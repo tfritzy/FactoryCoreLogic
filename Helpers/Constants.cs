07f7f946f6c2e494317bfb48ad5b6878e258ef27
Tyler Fritz <tfritzy@gmail.com>
2023-03-18 16:40:47 -0700

    Updating sizes

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -1,6 +1,6 @@
 public static class Constants
 {
-    public const float HEX_APOTHEM = .5f;
-    public const float HEX_RADIUS = 0.57735026918f;
+    public const float HEX_APOTHEM = 0.86602540378f;
+    public const float HEX_RADIUS = 1f;
     public const float HEX_HEIGHT = 1f;
 }
