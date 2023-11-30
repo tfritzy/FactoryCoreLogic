b3142215b5a0a36d6035a1747dd331e07ccdaa05
Tyler <tfritzy@gmail.com>
2023-11-30 06:18:39 -0800

    ?

--- Characters/Player.cs
+++ Characters/Player.cs
@@ -23,8 +23,8 @@ namespace Core
 
         protected override void InitComponents()
         {
-            this.SetComponent(new Inventory(this, 10, 14));
-            this.SetComponent(new ActiveItems(this, 10, 3));
+            this.SetComponent(new Inventory(this, 5, 8));
+            this.SetComponent(new ActiveItems(this, 8, 2));
             this.SetComponent(new WornItems(this, 1, 5));
         }
 
