37ccdec8f03c620d6b8ce3712e6b732293bb3a22
Tyler <tfritzy@gmail.com>
2024-01-18 21:25:15 -0800

    updating connection schema

--- Schema/MatchmakingServer/HostCreatingGame.cs
+++ Schema/MatchmakingServer/HostCreatingGame.cs
@@ -3,7 +3,5 @@ using Newtonsoft.Json;
 public class HostCreatingGame
 {
     public const string MessageType = "HostCreatingGame";
-
-    [JsonProperty("type")]
     public string Type = MessageType;
 }
